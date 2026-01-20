c530b3914f0a9f2623abab04be538aeec475f0dd
deblish-imac <x@deblish.com>
2026-01-20 23:04:21 +0200

    adding basic test for 00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,8 +6,21 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 17:38:36 by aapadill          #+#    #+#             */
-/*   Updated: 2026/01/14 17:38:37 by aapadill         ###   ########.fr       */
+/*   Updated: 2026/01/20 23:04:03 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+#include <iostream>
 
+static void test_construct_valid()
+{
+	std::cout << "\n -- test_construct_valid -- \n";
+	Bureaucrat a("Aaron", 42);
+	std::cout << a << std::endl;
+}
+
+int main()
+{
+	test_construct_valid();
+}
