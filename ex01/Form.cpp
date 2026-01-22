1cd2a6509e6896274a28651db2795cbba1773639
Aaron Padilla <aapadill@student.hive.fi>
2026-01-22 10:32:56 +0200

    moving to imac

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -6,8 +6,18 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 01:33:26 by aapadill          #+#    #+#             */
-/*   Updated: 2026/01/21 01:33:34 by aapadill         ###   ########.fr       */
+/*   Updated: 2026/01/22 10:32:00 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Form.hpp"
 
+Form::Form(){}
+
+Form::Form(std::string name, int gradeToSign, int gradeToExec){}
+
+Form::Form(const Form &other){}
+
+Form &Form::operator=(const Form &other){}
+
+Form::~Form(){}
